5fdb4f5b2b63def3f3965cf37574fc230a792792
Grabot <SanderKools@gmail.com>
2018-10-22 19:58:36 +0200

    Kotlin initial app code

---
+++ app/src/main/java/motivator/app/grabot/motiv8or/MainActivity.kt
@@ -0,0 +1,12 @@
+package motivator.app.grabot.motiv8or
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
