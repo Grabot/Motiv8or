648598e0d569a95030d5320e597ee3dc78e05b8c
Grabot <SanderKools@gmail.com>
2018-10-22 22:43:04 +0200

    added login screen initial design

--- app/src/main/java/motivator/app/grabot/motiv8or/LoginActivity.kt
+++ app/src/main/java/motivator/app/grabot/motiv8or/LoginActivity.kt
@@ -1,9 +1,9 @@
 package motivator.app.grabot.motiv8or
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
