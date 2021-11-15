4a2f9a024b3afa4daa0e0ffc153a995427fbe10b
Grabot <grabot@Grabots-Mac.local>
2021-11-15 05:43:57 -0800

    doing some update

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,7 +1,6 @@
 import UIKit
 import Flutter
 import Firebase
-import FirebaseMessaging
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -10,31 +9,9 @@ import FirebaseMessaging
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
-    pplication.registerForRemoteNotifications()
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
     override init() {
         FirebaseApp.configure()
     }
-    
-    override func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
-      Messaging.messaging().apnsToken = deviceToken
-      super.application(application, didRegisterForRemoteNotificationsWithDeviceToken: deviceToken)
-  }
-
-    override func application(_ application: UIApplication,
-        didReceiveRemoteNotification notification: [AnyHashable : Any],
-        fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
-        if Auth.auth().canHandleNotification(notification) {
-          completionHandler(.noData)
-          return
-        }
-    }
-
-    override func application(_ application: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any]) -> Bool {
-        if Auth.auth().canHandle(url) {
-          return true
-        }
-        return false;
-      }
 }
